dce7d8c94a5285aea5f21d5bc964f6b640847421
Hyago Philip <36174848+hyago1@users.noreply.github.com>
2021-06-29 13:15:27 -0300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,2 +1,18 @@
+/*async function getContent() {
+  try {
+    const dado = await fetch("https://nuweb.vercel.app/api/NuwebPlay");
+    const data = await dado.json();
+  } catch (error) {
+    console.log("DEU ERRO");
+  }
 
-     
+  getContent();
+}*/
+function dark() {
+  const fundo = document.querySelector(".s");
+  fundo.classList.add("s-activ");
+  const f = document.querySelector(".tituloesub");
+  f.classList.add("tituloesub-pp");
+  const fe = document.querySelector(".duracao");
+  fe.classList.add("duracao-pp");
+}
